3fe42a602543623c8a136f5b7b8dd66ceeba04d8
T.Gebauer <t-gebauer@users.noreply.github.com>
2021-05-14 20:40:38 +0200

    it works (kinda)
    Only checks whether the file is located inside of a git
    directory, not whether it is checked into version control.

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
+import * as git from "./include/git"
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// technically, only checks whether the file is located inside of a git directory, not whether it is actually version controlled...
+
 export function activate(context: vscode.ExtensionContext) {
+    console.log("activated")
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsc-auto-save" is now active!');
+    // The git extension is bundled with vscode and can not be uninstalled. But it could be disabled.
+    const gitExtension = vscode.extensions.getExtension<git.GitExtension>("vscode.git")
+    if (gitExtension === undefined) {
+        return vscode.window.showErrorMessage("Git extension not available")
+    }
+    const git = gitExtension.exports.getAPI(1)
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsc-auto-save.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    function isInsideGitRepository(uri: vscode.Uri): boolean {
+        return git.repositories.some((repository: git.Repository) => {
+            return uri.fsPath.startsWith(repository.rootUri.fsPath)
+        })
+    }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vsc-auto-save!');
-	});
-
-	context.subscriptions.push(disposable);
+    vscode.window.onDidChangeWindowState((windowState: vscode.WindowState) => {
+        if (!windowState.focused) {
+            // focus lost
+            vscode.workspace.textDocuments.forEach((document: vscode.TextDocument) => {
+                if (
+                    !document.isClosed &&
+                    !document.isUntitled &&
+                    document.isDirty &&
+                    isInsideGitRepository(document.uri)
+                ) {
+                    document.save()
+                }
+            })
+        }
+    }, context.subscriptions)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
